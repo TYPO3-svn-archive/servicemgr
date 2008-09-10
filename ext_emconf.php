4c6615936f51f977b80253d5af68a116843ab1c9
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-10 09:14:07 +0000

    reworked sql-file
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/servicemgr/trunk@11863 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "servicemgr"
 #
-# Auto generated 16-06-2008 11:27
+# Auto generated 09-09-2008 21:01
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -40,6 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:48:{s:9:"ChangeLog";s:4:"d1df";s:10:"README.txt";s:4:"ee2d";s:23:"class.tx_servicemgr.php";s:4:"95b6";s:32:"class.tx_servicemgr_download.php";s:4:"0a07";s:27:"class.tx_servicemgr_mp3.php";s:4:"8d35";s:21:"ext_conf_template.txt";s:4:"9005";s:12:"ext_icon.gif";s:4:"3238";s:17:"ext_localconf.php";s:4:"8873";s:15:"ext_php_api.dat";s:4:"aa7f";s:14:"ext_tables.php";s:4:"2747";s:14:"ext_tables.sql";s:4:"9f39";s:24:"ext_typoscript_setup.txt";s:4:"3230";s:35:"icon_tx_servicemgr_dutyschedule.gif";s:4:"475a";s:29:"icon_tx_servicemgr_events.gif";s:4:"b0c6";s:29:"icon_tx_servicemgr_series.gif";s:4:"9a80";s:30:"icon_tx_servicemgr_sermons.gif";s:4:"bdaa";s:33:"icon_tx_servicemgr_statistics.gif";s:4:"475a";s:27:"icon_tx_servicemgr_tags.gif";s:4:"4de0";s:31:"icon_tx_servicemgr_teamtype.gif";s:4:"3786";s:20:"locallang_common.xml";s:4:"8170";s:16:"locallang_db.xml";s:4:"b4e4";s:7:"tca.php";s:4:"3b43";s:31:"pi1/class.tx_servicemgr_pi1.php";s:4:"5d9c";s:17:"pi1/locallang.xml";s:4:"a86d";s:24:"pi1/static/editorcfg.txt";s:4:"0f7b";s:31:"pi2/class.tx_servicemgr_pi2.php";s:4:"5383";s:17:"pi2/locallang.xml";s:4:"2c27";s:24:"pi2/static/editorcfg.txt";s:4:"06c1";s:31:"pi3/class.tx_servicemgr_pi3.php";s:4:"ed9a";s:17:"pi3/locallang.xml";s:4:"5606";s:24:"pi3/static/editorcfg.txt";s:4:"1929";s:20:"pi3/static/setup.txt";s:4:"e9a9";s:31:"pi4/class.tx_servicemgr_pi4.php";s:4:"5113";s:17:"pi4/locallang.xml";s:4:"d8ce";s:24:"pi4/static/editorcfg.txt";s:4:"37ff";s:31:"pi5/class.tx_servicemgr_pi5.php";s:4:"d617";s:17:"pi5/locallang.xml";s:4:"7c94";s:24:"pi5/static/editorcfg.txt";s:4:"d85a";s:13:"res/error.gif";s:4:"f1aa";s:13:"res/error.png";s:4:"c847";s:19:"res/exclamation.gif";s:4:"98c8";s:19:"res/exclamation.png";s:4:"e4dd";s:15:"res/player1.swf";s:4:"d02b";s:14:"res/tables.css";s:4:"ed2a";s:15:"res/tables.html";s:4:"76fb";s:15:"res/tables.tmpl";s:4:"e849";s:41:"tests/tx_servicemgr_download_testcase.php";s:4:"7943";s:32:"tests/tx_servicemgr_testcase.php";s:4:"8901";}',
 );
 
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,251 +1,243 @@
+# TYPO3 Extension Manager dump 1.1
 #
-# Table structure for table 'tx_servicemgr_events'
+# Host: localhost    Database: typo3_420_final
+#--------------------------------------------------------
+
+
+#
+# Table structure for table "tx_servicemgr_events"
 #
 CREATE TABLE tx_servicemgr_events (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(30) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	datetime int(11) DEFAULT '0' NOT NULL,
-	subject varchar(255) DEFAULT '' NOT NULL,
-	public tinyint(3) DEFAULT '0' NOT NULL,
-	series int(11) DEFAULT '0' NOT NULL,
-	tags blob NOT NULL,
-	requiredteams blob NOT NULL,
-	documents blob NOT NULL,
-	notes text NOT NULL,
-	
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  t3ver_oid int(11) NOT NULL default '0',
+  t3ver_id int(11) NOT NULL default '0',
+  t3ver_wsid int(11) NOT NULL default '0',
+  t3ver_label varchar(30) NOT NULL default '',
+  t3ver_state tinyint(4) NOT NULL default '0',
+  t3ver_stage tinyint(4) NOT NULL default '0',
+  t3ver_count int(11) NOT NULL default '0',
+  t3ver_tstamp int(11) NOT NULL default '0',
+  t3_origuid int(11) NOT NULL default '0',
+  sys_language_uid int(11) NOT NULL default '0',
+  l18n_parent int(11) NOT NULL default '0',
+  l18n_diffsource mediumblob NOT NULL,
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  datetime int(11) NOT NULL default '0',
+  subject varchar(255) NOT NULL default '',
+  public tinyint(3) NOT NULL default '0',
+  series int(11) NOT NULL default '0',
+  tags blob NOT NULL,
+  requiredteams blob NOT NULL,
+  documents blob NOT NULL,
+  notes text NOT NULL,
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+  KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
-
 #
-# Table structure for table 'tx_servicemgr_series'
+# Table structure for table "tx_servicemgr_series"
 #
 CREATE TABLE tx_servicemgr_series (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(30) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	name varchar(255) DEFAULT '' NOT NULL,
-	
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  t3ver_oid int(11) NOT NULL default '0',
+  t3ver_id int(11) NOT NULL default '0',
+  t3ver_wsid int(11) NOT NULL default '0',
+  t3ver_label varchar(30) NOT NULL default '',
+  t3ver_state tinyint(4) NOT NULL default '0',
+  t3ver_stage tinyint(4) NOT NULL default '0',
+  t3ver_count int(11) NOT NULL default '0',
+  t3ver_tstamp int(11) NOT NULL default '0',
+  t3_origuid int(11) NOT NULL default '0',
+  sys_language_uid int(11) NOT NULL default '0',
+  l18n_parent int(11) NOT NULL default '0',
+  l18n_diffsource mediumblob NOT NULL,
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  name varchar(255) NOT NULL default '',
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+  KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
-
 #
-# Table structure for table 'tx_servicemgr_tags'
+# Table structure for table "tx_servicemgr_tags"
 #
 CREATE TABLE tx_servicemgr_tags (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(30) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	name varchar(255) DEFAULT '' NOT NULL,
-	parrent int(11) DEFAULT '0' NOT NULL,
-	
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  t3ver_oid int(11) NOT NULL default '0',
+  t3ver_id int(11) NOT NULL default '0',
+  t3ver_wsid int(11) NOT NULL default '0',
+  t3ver_label varchar(30) NOT NULL default '',
+  t3ver_state tinyint(4) NOT NULL default '0',
+  t3ver_stage tinyint(4) NOT NULL default '0',
+  t3ver_count int(11) NOT NULL default '0',
+  t3ver_tstamp int(11) NOT NULL default '0',
+  t3_origuid int(11) NOT NULL default '0',
+  sys_language_uid int(11) NOT NULL default '0',
+  l18n_parent int(11) NOT NULL default '0',
+  l18n_diffsource mediumblob NOT NULL,
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  name varchar(255) NOT NULL default '',
+  parrent int(11) NOT NULL default '0',
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+  KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
-
 #
-# Table structure for table 'tx_servicemgr_teamtype'
+# Table structure for table "tx_servicemgr_teamtype"
 #
 CREATE TABLE tx_servicemgr_teamtype (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(30) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	name varchar(255) DEFAULT '' NOT NULL,
-		
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  t3ver_oid int(11) NOT NULL default '0',
+  t3ver_id int(11) NOT NULL default '0',
+  t3ver_wsid int(11) NOT NULL default '0',
+  t3ver_label varchar(30) NOT NULL default '',
+  t3ver_state tinyint(4) NOT NULL default '0',
+  t3ver_stage tinyint(4) NOT NULL default '0',
+  t3ver_count int(11) NOT NULL default '0',
+  t3ver_tstamp int(11) NOT NULL default '0',
+  t3_origuid int(11) NOT NULL default '0',
+  sys_language_uid int(11) NOT NULL default '0',
+  l18n_parent int(11) NOT NULL default '0',
+  l18n_diffsource mediumblob NOT NULL,
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  name varchar(255) NOT NULL default '',
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+  KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
-
 #
-# Table structure for table 'tx_servicemgr_dutyschedule'
+# Table structure for table "tx_servicemgr_dutyschedule"
 #
 CREATE TABLE tx_servicemgr_dutyschedule (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(30) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	event int(11) DEFAULT '0' NOT NULL,
-	duty blob NOT NULL,
-		
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  t3ver_oid int(11) NOT NULL default '0',
+  t3ver_id int(11) NOT NULL default '0',
+  t3ver_wsid int(11) NOT NULL default '0',
+  t3ver_label varchar(30) NOT NULL default '',
+  t3ver_state tinyint(4) NOT NULL default '0',
+  t3ver_stage tinyint(4) NOT NULL default '0',
+  t3ver_count int(11) NOT NULL default '0',
+  t3ver_tstamp int(11) NOT NULL default '0',
+  t3_origuid int(11) NOT NULL default '0',
+  sys_language_uid int(11) NOT NULL default '0',
+  l18n_parent int(11) NOT NULL default '0',
+  l18n_diffsource mediumblob NOT NULL,
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  event int(11) NOT NULL default '0',
+  duty blob NOT NULL,
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+  KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
-
 #
-# Table structure for table 'tx_servicemgr_statistics'
+# Table structure for table "tx_servicemgr_statistics"
 #
 CREATE TABLE tx_servicemgr_statistics (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	recordid blob NOT NULL,
-	type int(11) DEFAULT '0' NOT NULL,
-	hit tinytext NOT NULL,
-	
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  recordid blob NOT NULL,
+  type int(11) NOT NULL default '0',
+  hit tinytext NOT NULL,
+  PRIMARY KEY (uid),
+  KEY parent (pid)
 );
 
 
-
 #
-# Table structure for table 'tx_servicemgr_sermons'
+# Table structure for table "tx_servicemgr_sermons"
 #
 CREATE TABLE tx_servicemgr_sermons (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(30) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3_origuid int(11) DEFAULT '0' NOT NULL,
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l18n_parent int(11) DEFAULT '0' NOT NULL,
-	l18n_diffsource mediumblob NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	title varchar(255) DEFAULT '' NOT NULL,
-	file varchar(255) DEFAULT '' NOT NULL,
-	filedate int(11) DEFAULT '0' NOT NULL,
-	playtime int(11) DEFAULT '0' NOT NULL,
-	filesize int(11) DEFAULT '0' NOT NULL,
-	mimetype varchar(255) DEFAULT '' NOT NULL,
-	bitrate int(11) DEFAULT '0' NOT NULL,
-	album varchar(255) DEFAULT '' NOT NULL,
-	
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL default '0',
+  tstamp int(11) NOT NULL default '0',
+  crdate int(11) NOT NULL default '0',
+  cruser_id int(11) NOT NULL default '0',
+  t3ver_oid int(11) NOT NULL default '0',
+  t3ver_id int(11) NOT NULL default '0',
+  t3ver_wsid int(11) NOT NULL default '0',
+  t3ver_label varchar(30) NOT NULL default '',
+  t3ver_state tinyint(4) NOT NULL default '0',
+  t3ver_stage tinyint(4) NOT NULL default '0',
+  t3ver_count int(11) NOT NULL default '0',
+  t3ver_tstamp int(11) NOT NULL default '0',
+  t3_origuid int(11) NOT NULL default '0',
+  sys_language_uid int(11) NOT NULL default '0',
+  l18n_parent int(11) NOT NULL default '0',
+  l18n_diffsource mediumblob NOT NULL,
+  deleted tinyint(4) NOT NULL default '0',
+  hidden tinyint(4) NOT NULL default '0',
+  event int(11) NOT NULL default '0',
+  title varchar(255) NOT NULL default '',
+  file varchar(255) default '',
+  filedate int(11) default '0',
+  playtime int(11) default '0',
+  bitrate int(11) default '0',
+  album varchar(255) default '',
+  filesize int(11) NOT NULL default '0',
+  mimetype varchar(255) NOT NULL default '',
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+  KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
-
 #
-# Table structure for table 'fe_users'
+# Table structure for table "fe_users"
 #
 CREATE TABLE fe_users (
-	tx_servicemgr_image blob NOT NULL,
-	tx_servicemgr_description text NOT NULL
+  tx_servicemgr_image blob NOT NULL,
+  tx_servicemgr_description text NOT NULL,
 );
 
 
-
 #
-# Table structure for table 'fe_groups'
+# Table structure for table "fe_groups"
 #
 CREATE TABLE fe_groups (
-	tx_servicemgr_category int(11) DEFAULT '0' NOT NULL,
-	tx_servicemgr_dutyschedule tinyint(3) DEFAULT '0' NOT NULL, 
-	tx_servicemgr_leaders blob NOT NULL,
-	tx_servicemgr_isteam tinyint(3) DEFAULT '0' NOT NULL,
-	tx_servicemgr_image blob NOT NULL,
-	tx_servicemgr_description text NOT NULL,
-	tx_servicemgr_asteaminschedule tinyint(3) DEFAULT '0' NOT NULL
+  tx_servicemgr_category int(11) NOT NULL default '0',
+  tx_servicemgr_dutyschedule tinyint(3) NOT NULL default '0',
+  tx_servicemgr_leaders blob NOT NULL,
+  tx_servicemgr_isteam tinyint(3) NOT NULL default '0',
+  tx_servicemgr_image blob NOT NULL,
+  tx_servicemgr_description text NOT NULL,
+  tx_servicemgr_asteaminschedule tinyint(3) NOT NULL default '0',
 );
