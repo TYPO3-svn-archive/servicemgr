b6991e942ba5277175d2f94f6a29ba50d8b7d435
peschuster <peschuster@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-01 16:01:41 +0000

    shifted table field fe_users:description to other extension
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/servicemgr/trunk@13645 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,17 +2,17 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 $TCA["tx_servicemgr_events"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_events',		
-		'label'     => 'subject',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_events',
+		'label'     => 'subject',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l18n_parent',	
-		'transOrigDiffSourceField' => 'l18n_diffsource',	
-		'default_sortby' => "ORDER BY datetime DESC",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'languageField'            => 'sys_language_uid',
+		'transOrigPointerField'    => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'default_sortby' => "ORDER BY datetime DESC",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -25,17 +25,17 @@ $TCA["tx_servicemgr_events"] = array (
 
 $TCA["tx_servicemgr_series"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_series',		
-		'label'     => 'name',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_series',
+		'label'     => 'name',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l18n_parent',	
-		'transOrigDiffSourceField' => 'l18n_diffsource',	
-		'default_sortby' => "ORDER BY crdate",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'languageField'            => 'sys_language_uid',
+		'transOrigPointerField'    => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'default_sortby' => "ORDER BY crdate",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -48,17 +48,17 @@ $TCA["tx_servicemgr_series"] = array (
 
 $TCA["tx_servicemgr_tags"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_tags',		
-		'label'     => 'name',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_tags',
+		'label'     => 'name',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l18n_parent',	
-		'transOrigDiffSourceField' => 'l18n_diffsource',	
-		'default_sortby' => "ORDER BY crdate",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'languageField'            => 'sys_language_uid',
+		'transOrigPointerField'    => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'default_sortby' => "ORDER BY crdate",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -71,17 +71,17 @@ $TCA["tx_servicemgr_tags"] = array (
 
 $TCA["tx_servicemgr_teamtype"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_teamtype',		
-		'label'     => 'name',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_teamtype',
+		'label'     => 'name',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l18n_parent',	
-		'transOrigDiffSourceField' => 'l18n_diffsource',	
-		'default_sortby' => "ORDER BY crdate",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'languageField'            => 'sys_language_uid',
+		'transOrigPointerField'    => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'default_sortby' => "ORDER BY crdate",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -94,17 +94,17 @@ $TCA["tx_servicemgr_teamtype"] = array (
 
 $TCA["tx_servicemgr_dutyschedule"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_dutyschedule',		
-		'label'     => 'event',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_dutyschedule',
+		'label'     => 'event',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l18n_parent',	
-		'transOrigDiffSourceField' => 'l18n_diffsource',	
-		'default_sortby' => "ORDER BY crdate",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'languageField'            => 'sys_language_uid',
+		'transOrigPointerField'    => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'default_sortby' => "ORDER BY crdate",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -117,14 +117,14 @@ $TCA["tx_servicemgr_dutyschedule"] = array (
 
 $TCA["tx_servicemgr_statistics"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_statistics',		
-		'label'     => 'recordid',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_statistics',
+		'label'     => 'recordid',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'default_sortby' => "ORDER BY crdate",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'default_sortby' => "ORDER BY crdate",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -137,17 +137,17 @@ $TCA["tx_servicemgr_statistics"] = array (
 
 $TCA["tx_servicemgr_sermons"] = array (
 	"ctrl" => array (
-		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_sermons',		
-		'label'     => 'title',	
+		'title'     => 'LLL:EXT:servicemgr/locallang_db.xml:tx_servicemgr_sermons',
+		'label'     => 'title',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'languageField'            => 'sys_language_uid',	
-		'transOrigPointerField'    => 'l18n_parent',	
-		'transOrigDiffSourceField' => 'l18n_diffsource',	
-		'default_sortby' => "ORDER BY crdate",	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'languageField'            => 'sys_language_uid',
+		'transOrigPointerField'    => 'l18n_parent',
+		'transOrigDiffSourceField' => 'l18n_diffsource',
+		'default_sortby' => "ORDER BY crdate",
+		'delete' => 'deleted',
+		'enablecolumns' => array (
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -159,94 +159,77 @@ $TCA["tx_servicemgr_sermons"] = array (
 );
 
 $tempColumns = Array (
-	"tx_servicemgr_description" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_users.tx_servicemgr_description",		
+	"tx_servicemgr_leaders" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_leaders",
 		"config" => Array (
-			"type" => "text",
-			"cols" => "30",	
-			"rows" => "5",
-		)
-	),
-);
-
-
-t3lib_div::loadTCA("fe_users");
-t3lib_extMgm::addTCAcolumns("fe_users",$tempColumns,1);
-t3lib_extMgm::addToAllTCAtypes("fe_users",";;;;1-1-1, tx_servicemgr_description");
-
-$tempColumns = Array (
-	"tx_servicemgr_leaders" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_leaders",		
-		"config" => Array (
-			"type" => "select",	
-			"foreign_table" => "fe_users",	
-			"foreign_table_where" => "AND fe_users.usergroup LIKE '%###THIS_UID###%' ORDER BY fe_users.uid",	
-			"size" => 10,	
+			"type" => "select",
+			"foreign_table" => "fe_users",
+			"foreign_table_where" => "AND fe_users.usergroup LIKE '%###THIS_UID###%' ORDER BY fe_users.uid",
+			"size" => 10,
 			"minitems" => 0,
 			"maxitems" => 10,
 		)
 	),
-	"tx_servicemgr_category" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_categroy",		
+	"tx_servicemgr_category" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_categroy",
 		"config" => Array (
-			"type" => "select",	
-			"foreign_table" => "tx_servicemgr_teamtype",	
-			"foreign_table_where" => "ORDER BY tx_servicemgr_teamtype.uid",	
-			"size" => 1,	
+			"type" => "select",
+			"foreign_table" => "tx_servicemgr_teamtype",
+			"foreign_table_where" => "ORDER BY tx_servicemgr_teamtype.uid",
+			"size" => 1,
 			"minitems" => 0,
 			"maxitems" => 1,
 		)
 	),
-	"tx_servicemgr_isteam" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_isteam",		
+	"tx_servicemgr_isteam" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_isteam",
 		"config" => Array (
 			"type" => "check",
 		)
 	),
-	"tx_servicemgr_publicteam" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_publicteam",		
+	"tx_servicemgr_publicteam" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_publicteam",
 		"config" => Array (
 			"type" => "check",
 		)
 	),
-	"tx_servicemgr_dutyschedule" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_dutyschedule",		
+	"tx_servicemgr_dutyschedule" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_dutyschedule",
 		"config" => Array (
 			"type" => "check",
 		)
 	),
-	"tx_servicemgr_dsname" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_dsname",		
+	"tx_servicemgr_dsname" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_dsname",
 		"config" => Array (
-			"type" => "input",	
-			"size" => "30",	
+			"type" => "input",
+			"size" => "30",
 			"eval" => "trim",
 		)
 	),
-	"tx_servicemgr_asteaminschedule" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_asteaminschedule",		
+	"tx_servicemgr_asteaminschedule" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_asteaminschedule",
 		"config" => Array (
 			"type" => "check",
 		)
 	),
-	"tx_servicemgr_image" => Array (		
-		"exclude" => 1,		
-		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_image",		
+	"tx_servicemgr_image" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:servicemgr/locallang_db.xml:fe_groups.tx_servicemgr_image",
 		"config" => Array (
 			"type" => "group",
 			"internal_type" => "file",
-			"allowed" => $GLOBALS["TYPO3_CONF_VARS"]["GFX"]["imagefile_ext"],	
-			"max_size" => 500,	
+			"allowed" => $GLOBALS["TYPO3_CONF_VARS"]["GFX"]["imagefile_ext"],
+			"max_size" => 500,
 			"uploadfolder" => "uploads/tx_servicemgr",
-			"size" => 1,	
+			"size" => 1,
 			"minitems" => 0,
 			"maxitems" => 1,
 		)
--- ext_tables.sql
+++ ext_tables.sql
@@ -8,28 +8,28 @@
 # Table structure for table "tx_servicemgr_events"
 #
 CREATE TABLE tx_servicemgr_events (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  sys_language_uid int(11) NOT NULL default '0',
-  l18n_parent int(11) NOT NULL default '0',
-  l18n_diffsource mediumblob NOT NULL,
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  datetime int(11) NOT NULL default '0',
-  subject varchar(255) NOT NULL default '',
-  public tinyint(3) NOT NULL default '0',
-  series int(11) NOT NULL default '0',
-  tags tinytext NOT NULL,
-  requiredteams tinytext NOT NULL,
-  dutyscheduleopen tinyint(4) NOT NULL default '1',
-  documents blob NOT NULL,
-  notes text NOT NULL,
-  notes_internal text NOT NULL,
-  PRIMARY KEY (uid),
-  KEY parent (pid),
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	sys_language_uid int(11) NOT NULL default '0',
+	l18n_parent int(11) NOT NULL default '0',
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	datetime int(11) NOT NULL default '0',
+	subject varchar(255) NOT NULL default '',
+	public tinyint(3) NOT NULL default '0',
+	series int(11) NOT NULL default '0',
+	tags tinytext NOT NULL,
+	requiredteams tinytext NOT NULL,
+	dutyscheduleopen tinyint(4) NOT NULL default '1',
+	documents blob NOT NULL,
+	notes text NOT NULL,
+	notes_internal text NOT NULL,
+	PRIMARY KEY (uid),
+	KEY parent (pid),
 );
 
 
@@ -37,19 +37,19 @@ CREATE TABLE tx_servicemgr_events (
 # Table structure for table "tx_servicemgr_series"
 #
 CREATE TABLE tx_servicemgr_series (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  sys_language_uid int(11) NOT NULL default '0',
-  l18n_parent int(11) NOT NULL default '0',
-  l18n_diffsource mediumblob NOT NULL,
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  name varchar(255) NOT NULL default '',
-  PRIMARY KEY (uid),
-  KEY parent (pid),
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	sys_language_uid int(11) NOT NULL default '0',
+	l18n_parent int(11) NOT NULL default '0',
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	name varchar(255) NOT NULL default '',
+	PRIMARY KEY (uid),
+	KEY parent (pid),
 );
 
 
@@ -57,20 +57,20 @@ CREATE TABLE tx_servicemgr_series (
 # Table structure for table "tx_servicemgr_tags"
 #
 CREATE TABLE tx_servicemgr_tags (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  sys_language_uid int(11) NOT NULL default '0',
-  l18n_parent int(11) NOT NULL default '0',
-  l18n_diffsource mediumblob NOT NULL,
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  name varchar(255) NOT NULL default '',
-  parrent int(11) NOT NULL default '0',
-  PRIMARY KEY (uid),
-  KEY parent (pid),
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	sys_language_uid int(11) NOT NULL default '0',
+	l18n_parent int(11) NOT NULL default '0',
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	name varchar(255) NOT NULL default '',
+	parrent int(11) NOT NULL default '0',
+	PRIMARY KEY (uid),
+	KEY parent (pid),
 );
 
 
@@ -78,19 +78,19 @@ CREATE TABLE tx_servicemgr_tags (
 # Table structure for table "tx_servicemgr_teamtype"
 #
 CREATE TABLE tx_servicemgr_teamtype (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  sys_language_uid int(11) NOT NULL default '0',
-  l18n_parent int(11) NOT NULL default '0',
-  l18n_diffsource mediumblob NOT NULL,
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  name varchar(255) NOT NULL default '',
-  PRIMARY KEY (uid),
-  KEY parent (pid),
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	sys_language_uid int(11) NOT NULL default '0',
+	l18n_parent int(11) NOT NULL default '0',
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	name varchar(255) NOT NULL default '',
+	PRIMARY KEY (uid),
+	KEY parent (pid),
 );
 
 
@@ -98,20 +98,20 @@ CREATE TABLE tx_servicemgr_teamtype (
 # Table structure for table "tx_servicemgr_dutyschedule"
 #
 CREATE TABLE tx_servicemgr_dutyschedule (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  sys_language_uid int(11) NOT NULL default '0',
-  l18n_parent int(11) NOT NULL default '0',
-  l18n_diffsource mediumblob NOT NULL,
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  event int(11) NOT NULL default '0',
-  duty blob NOT NULL,
-  PRIMARY KEY (uid),
-  KEY parent (pid),
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	sys_language_uid int(11) NOT NULL default '0',
+	l18n_parent int(11) NOT NULL default '0',
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	event int(11) NOT NULL default '0',
+	duty blob NOT NULL,
+	PRIMARY KEY (uid),
+	KEY parent (pid),
 );
 
 
@@ -119,18 +119,18 @@ CREATE TABLE tx_servicemgr_dutyschedule (
 # Table structure for table "tx_servicemgr_statistics"
 #
 CREATE TABLE tx_servicemgr_statistics (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  recordid blob NOT NULL,
-  type int(11) NOT NULL default '0',
-  hit tinytext NOT NULL,
-  PRIMARY KEY (uid),
-  KEY parent (pid)
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	recordid blob NOT NULL,
+	type int(11) NOT NULL default '0',
+	hit tinytext NOT NULL,
+	PRIMARY KEY (uid),
+	KEY parent (pid)
 );
 
 
@@ -138,48 +138,39 @@ CREATE TABLE tx_servicemgr_statistics (
 # Table structure for table "tx_servicemgr_sermons"
 #
 CREATE TABLE tx_servicemgr_sermons (
-  uid int(11) NOT NULL auto_increment,
-  pid int(11) NOT NULL default '0',
-  tstamp int(11) NOT NULL default '0',
-  crdate int(11) NOT NULL default '0',
-  cruser_id int(11) NOT NULL default '0',
-  sys_language_uid int(11) NOT NULL default '0',
-  l18n_parent int(11) NOT NULL default '0',
-  l18n_diffsource mediumblob NOT NULL,
-  deleted tinyint(4) NOT NULL default '0',
-  hidden tinyint(4) NOT NULL default '0',
-  event int(11) NOT NULL default '0',
-  title varchar(255) NOT NULL default '',
-  file varchar(255) default '',
-  filedate int(11) default '0',
-  playtime int(11) default '0',
-  bitrate int(11) default '0',
-  album varchar(255) default '',
-  filesize int(11) NOT NULL default '0',
-  mimetype varchar(255) NOT NULL default '',
-  PRIMARY KEY (uid),
-  KEY parent (pid),
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) NOT NULL default '0',
+	tstamp int(11) NOT NULL default '0',
+	crdate int(11) NOT NULL default '0',
+	cruser_id int(11) NOT NULL default '0',
+	sys_language_uid int(11) NOT NULL default '0',
+	l18n_parent int(11) NOT NULL default '0',
+	l18n_diffsource mediumblob NOT NULL,
+	deleted tinyint(4) NOT NULL default '0',
+	hidden tinyint(4) NOT NULL default '0',
+	event int(11) NOT NULL default '0',
+	title varchar(255) NOT NULL default '',
+	file varchar(255) default '',
+	filedate int(11) default '0',
+	playtime int(11) default '0',
+	bitrate int(11) default '0',
+	album varchar(255) default '',
+	filesize int(11) NOT NULL default '0',
+	mimetype varchar(255) NOT NULL default '',
+	PRIMARY KEY (uid),
+	KEY parent (pid),
 );
-
-
-#
-# Table structure for table "fe_users"
-#
-CREATE TABLE fe_users (
-  tx_servicemgr_description text NOT NULL,
-);
-
 
 #
 # Table structure for table "fe_groups"
 #
 CREATE TABLE fe_groups (
-  tx_servicemgr_category int(11) NOT NULL default '0',
-  tx_servicemgr_dutyschedule tinyint(3) NOT NULL default '0',
-  tx_servicemgr_leaders blob NOT NULL,
-  tx_servicemgr_isteam tinyint(3) NOT NULL default '0',
-  tx_servicemgr_publicteam tinyint(3) NOT NULL default '0',
-  tx_servicemgr_image blob NOT NULL,
-  tx_servicemgr_asteaminschedule tinyint(3) NOT NULL default '0',
-  tx_servicemgr_dsname  varchar(30) NOT NULL default '',
+	tx_servicemgr_category int(11) NOT NULL default '0',
+	tx_servicemgr_dutyschedule tinyint(3) NOT NULL default '0',
+	tx_servicemgr_leaders blob NOT NULL,
+	tx_servicemgr_isteam tinyint(3) NOT NULL default '0',
+	tx_servicemgr_publicteam tinyint(3) NOT NULL default '0',
+	tx_servicemgr_image blob NOT NULL,
+	tx_servicemgr_asteaminschedule tinyint(3) NOT NULL default '0',
+	tx_servicemgr_dsname  varchar(30) NOT NULL default '',
 );
